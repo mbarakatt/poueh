a8a117add56c6c8b9595da432f8de6e5ea50672d
Maxime Barakatt <mbarakatt@gmail.com>
2018-01-29 10:33:45 -0500

    Ajuste angle de rotation et la position d'une balle

--- Assets/_Completed-Game/Scripts/Rotator.cs
+++ Assets/_Completed-Game/Scripts/Rotator.cs
@@ -6,9 +6,9 @@ public class Rotator : MonoBehaviour {
 	// Before rendering each frame..
 	void Update () 
 	{
-		// Rotate the game object that this script is attached to by 15 in the X axis,
+		// Rotate the game object that this script is attached to by 10 in the X axis,
 		// 30 in the Y axis and 45 in the Z axis, multiplied by deltaTime in order to make it per second
 		// rather than per frame.
-		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+		transform.Rotate (new Vector3 (10, 30, 45) * Time.deltaTime);
 	}
 }	
